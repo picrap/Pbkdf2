98a4f082624a93cb2173da56d9d623b9a5de093e
pascal <pascal@craponne.fr>
2022-11-20 21:06:22 +0100

    Fixed typo

--- Pbkdf2Test/Reference.cs
+++ Pbkdf2Test/Reference.cs
@@ -22,12 +22,12 @@ public class Reference
     [TestCase("SHA1", "pw", "salt", 10, 199)]
 
     [TestCase("SHA512", "pwd", "salt!", 10, 201)]
-    public void CompareHMACImplementations(string algorithmName, string password, string salt, int interations, int desiredKeyLength)
+    public void CompareHMACImplementations(string algorithmName, string password, string salt, int iterations, int desiredKeyLength)
     {
         var passwordBytes = Encoding.UTF8.GetBytes(password);
         var saltBytes = Encoding.UTF8.GetBytes(salt);
-        var reference = Rfc2898DeriveBytes.Pbkdf2(passwordBytes, saltBytes, interations, new HashAlgorithmName(algorithmName), desiredKeyLength);
-        var testedImplementation = Pbkdf2.Compute("HMAC" + algorithmName, passwordBytes, saltBytes, interations, desiredKeyLength);
+        var reference = Rfc2898DeriveBytes.Pbkdf2(passwordBytes, saltBytes, iterations, new HashAlgorithmName(algorithmName), desiredKeyLength);
+        var testedImplementation = Pbkdf2.Compute("HMAC" + algorithmName, passwordBytes, saltBytes, iterations, desiredKeyLength);
         Assert.That(reference, Is.EqualTo(testedImplementation));
     }
 }
