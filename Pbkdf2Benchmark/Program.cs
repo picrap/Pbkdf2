b15668c224397de5a12106a9a207e034fcc92a85
pascal <pascal@craponne.fr>
2022-11-20 22:49:11 +0100

    Added benchmark application

---
+++ Pbkdf2Benchmark/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using System.Text;
+
+var algorithmName = "SHA512";
+var passwordBytes = Encoding.UTF8.GetBytes("password");
+var saltBytes = Encoding.UTF8.GetBytes("salt");
+int iterations = 100_000;
+var desiredKeyLength = 32 << 10;
+
+var testedImplementation = Pbkdf2.Pbkdf2.Compute("HMAC" + algorithmName, passwordBytes, saltBytes, iterations, desiredKeyLength);
